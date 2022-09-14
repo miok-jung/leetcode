4198cc21377247b77f37965f86e7831de880f155
miok-jung <miok.dev@gmail.com>
2022-09-14 22:24:42 +0900

    Time: 80 ms (78.13%), Space: 41.8 MB (57.15%) - LeetHub

---
+++ 509-fibonacci-number/509-fibonacci-number.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var fib = function(n) {
+    let a = 0, b = 1, c = n;
+    
+    for(let i = 2; i <= n; i++){
+        c = a + b;
+        a = b;
+        b = c;
+    }
+    
+    return c;
+};

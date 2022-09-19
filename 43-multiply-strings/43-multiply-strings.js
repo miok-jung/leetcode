2837b0cb5b5fcdbc50348fa655e22a25c46afd44
miok-jung <miok.dev@gmail.com>
2022-09-19 23:01:12 +0900

    Time: 95 ms (76.04%), Space: 41.5 MB (99.71%) - LeetHub

---
+++ 43-multiply-strings/43-multiply-strings.js
@@ -0,0 +1,11 @@
+/**
+ * @param {string} num1
+ * @param {string} num2
+ * @return {string}
+ */
+var multiply = function(num1, num2) {
+  const a = BigInt(num1);
+  const b = BigInt(num2);
+  
+  return (a * b).toString();
+};
